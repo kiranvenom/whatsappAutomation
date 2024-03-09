01449e4814f2622841b4330cabaeecba84655d8d
Kiran <km060360@gmail.com>
2024-03-09 19:26:43 +0530

    upload section

--- client/src/Components/Nav.jsx
+++ client/src/Components/Nav.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { IoLogoWhatsapp } from 'react-icons/io';
 
 const Nav = () => {
 	let userEmail = localStorage.getItem('userEmail');
@@ -9,13 +10,20 @@ const Nav = () => {
 		window.location.reload();
 	};
 
+	let formatedName = userEmail?.split('')?.splice(0, 2)?.join('');
+
 	return (
 		<>
 			<nav className='max-w-[1200px] m-auto flex justify-between h-[4rem] items-center border-b'>
-				<h1>Awesome</h1>
+				<div className='flex items-center gap-2'>
+					<h1 className='text-green-500'>WhatsApp Automation</h1>
+					<IoLogoWhatsapp size={30} color='lightgreen' />
+				</div>
 				{userEmail !== null ? (
 					<div className='flex gap-4 items-center'>
-						<h1>{userEmail}</h1>
+						<h1 className='border uppercase w-[35px] h-[35px] rounded-full flex justify-center items-center'>
+							{formatedName}
+						</h1>
 						<button
 							onClick={handleLogOut}
 							className='bg-blue-500 px-4 py-1 rounded-md ml-4 text-white'>
